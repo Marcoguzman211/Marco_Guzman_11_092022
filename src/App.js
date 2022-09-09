8cee92e8c3be690be9ba022a46978821b9cad068
Marcoguzman211 <guzmanescobar3@gmail.com>
2022-09-09 22:33:08 +0200

    Mise en place scss et react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import Home from './pages/Home'
+import Logement from './pages/Logement'
+import NotFound from './pages/NotFound'
+import About from './pages/About'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./css/main.css";
+
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Header</h1>
-      <p>Hola</p>
+      <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/logement/:id" element={<Logement />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
